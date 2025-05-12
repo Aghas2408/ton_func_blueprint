787b31d0faf5dbe4a91879a893f317c332234a62
hexens-aghas <aghasi.seyranyan@hexens.io>
2025-05-12 16:25:43 +0400

    updated some names

--- scripts/incrementCounter.ts
+++ scripts/incrementCounter.ts
@@ -5,7 +5,7 @@ import { NetworkProvider, sleep } from '@ton/blueprint';
 export async function run(provider: NetworkProvider, args: string[]) {
     const ui = provider.ui();
 
-    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('HelloWorld address'));
+    const address = Address.parse(args.length > 0 ? args[0] : await ui.input('Counter address'));
 
     if (!(await provider.isContractDeployed(address))) {
         ui.write(`Error: Contract at address ${address} is not deployed!`);
